90e8c2c87fe217a246c6a53d8c7b7f309bc9d264
r0th3r <503407184@qq.com>
2022-07-12 21:29:19 +0800

    vftable helper

---
+++ vtbl.js
@@ -0,0 +1,58 @@
+
+/*
+
+    vftable (Alpha)
+
+*/
+
+"use strict";
+
+
+
+/* Virtual Table Swap (VTableSwap) */
+class vftableHelper {
+
+    #vftable_shadow_ = [ NULL, NULL ];
+
+    constructor(instance) {
+        this.this_ptr_ = ptr(instance);
+
+        /* vftable dump */
+        this.#vftable_shadow_[0] = this.vftable();
+        this.#vftable_shadow_[1] = Memory.dup(
+            this.vftable(), 
+            Process.pointerSize * this.#vftable_count());
+    }
+
+    get() { return this.this_ptr_; }
+
+    vftable() {
+        return this.get().readPointer();
+    }
+
+    attach() {
+        this.get().writePointer( this.#vftable_shadow_[1] );
+    }
+
+    revert() {
+        this.get().writePointer( this.#vftable_shadow_[0] );
+        this.#vftable_shadow_[1] = NULL;
+    }
+
+    #vftable_count(limit_count=0) {
+        let limit_count_ = limit_count;
+
+        if (0 == limit_count)
+            limit_count_ = int64(Process.pageSize / Process.pointerSize);
+
+        let count_ = 0; 
+        do {
+            let empty_ = this.vftable().add( count_ *  Process.pointerSize).readPointer();
+            /* eof */
+            if (empty_.isNull() || null == Process.findRangeByAddress(empty_)) break;
+            count_ += 1;
+        } while (count_ < limit_count_);
+
+        return count_;
+    }
+}
