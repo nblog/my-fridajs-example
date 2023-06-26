9168ad5335c547238fa2fab27a967521b37b5fef
共产主义接班人 <503407184@qq.com>
2023-06-26 12:22:22 +0800

    Update pe-dumper.js

--- pe-dumper.js
+++ pe-dumper.js
@@ -1,4 +1,27 @@
 
+function size_of_image(addr=NULL) {
+    let nt_addr = addr.add(addr.add(0x3C).readU32());
+    let nt_header = nt_addr.readU32();
+    if (nt_header != 0x4550) return '0';
+
+    let file_addr = nt_addr.add(0x4);
+    let file_header = file_addr.readByteArray(0x14);
+
+    let opt_addr = file_addr.add(0x14);
+    let opt_header = opt_addr.readByteArray(0x60);
+
+    let range_length = function(addr) {
+        for (const range of Process.enumerateRanges('r--')) {
+            if (range.base.equals(addr)) {
+                return range.size;
+            }
+        };
+    }
+
+    let length = Math.max(opt_addr.add(0x38).readU32(), range_length(addr));
+    return `${length.toString(16)}`;
+}
+
 rpc.exports.scan = function () {
     for (const range of Process.enumerateRanges('r--')) {
         /* 'This program cannot be run in DOS mode' */
@@ -12,10 +35,10 @@ rpc.exports.scan = function () {
                 let addr = match[index].address.sub(78);
 
                 if (!match[index].address.and(255).equals(78)) {                    
-                    console.log(`embedded: [${0}]  ${addr}`);
+                    console.log(`embedded: [${size_of_image(addr)}]  ${addr}`);
                 }
                 else if (Process.findModuleByAddress(addr) === null) {
-                    console.log(`mapping: [${0}]  ${addr}`);
+                    console.log(`mapping: [${size_of_image(addr)}]  ${addr}`);
                 }
             }
         } catch (error) { continue; }
