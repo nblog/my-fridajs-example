48581d58b7bf6a8428a0bb58fb7ab9067c4ac219
共产主义接班人 <503407184@qq.com>
2024-07-10 15:25:14 +0800

    Update trace-file.js

--- trace-file.js
+++ trace-file.js
@@ -5,25 +5,41 @@
 var cfg = {
     preview_length: -1, // `-1` for all
     opened_files: new Map/*<NativePointer, String>*/(), // <FileHandle, FileName>
+    has_name: function (hFile) {
+        return String(cfg.opened_files.has(Number(hFile)) ? 
+            `\"${cfg.opened_files.get(Number(hFile))}\"` : hFile.toString(16));
+    }
 };
 
-
-function has_name(hObject) {
-    const hFile = Number(hObject);
-    return cfg.opened_files.has(hFile) ? 
-        `\"${cfg.opened_files.get(hFile)}\"` : hObject.toString(16);
+class utils {
+    static current_pointer(hFile) {
+        const FILE_CURRENT = 1;
+        const lpliNew = Memory.alloc(8);
+    
+        return new NativeFunction(
+            Module.getExportByName('kernel32.dll', 'SetFilePointerEx'),
+            'bool', ['pointer', 'int64', 'pointer', 'int32'])
+            (hFile, 0, lpliNew, FILE_CURRENT) ? lpliNew.readS64() : 0;
+    }
 }
 
-function current_pointer(hFile) {
-    const FILE_CURRENT = 1;
-    const lpliNew = Memory.alloc(8);
+class UNICODE_STRING {
+    constructor(UString=NULL) {
+        this.Length = 0;
+        this.MaximumLength = 0;
+        this.Buffer = NULL;
 
-    return new NativeFunction(
-        Module.getExportByName('kernel32.dll', 'SetFilePointerEx'),
-        'bool', ['pointer', 'int64', 'pointer', 'int32'])
-        (hFile, 0, lpliNew, FILE_CURRENT) ? lpliNew.readS64() : 0;
+        if (!UString || UString.isNull()) return;
+
+        this.Length = UString.add(0).readU16();
+        this.MaximumLength = UString.add(2).readU16();
+        this.Buffer = UString.add(Process.pointerSize).readPointer();
+    }
+    toString() {
+        if (this.Length === 0 || this.Buffer.isNull()) return '';
+        return this.Buffer.readUtf16String();
+    }
 }
-
 
 Interceptor.attach(Module.getExportByName('kernel32.dll', 'CloseHandle'), {
     onEnter: function (args) {
@@ -43,11 +59,12 @@ Interceptor.attach(Module.getExportByName('ntdll.dll', 'ZwCreateFile'), {
             const lpObjectAttributes = args[2];
             if (lpObjectAttributes.equals(NULL)) return;
 
-            const ObjectName = lpObjectAttributes.add(2 * Process.pointerSize).readPointer();
-            if (ObjectName.equals(NULL) && 0 == ObjectName.add(0).readU16()) return;
+            const OBJECT_ATTRIBUTES = {
+                Length: lpObjectAttributes.add(0 * Process.pointerSize).readU32(),
+                ObjectName: lpObjectAttributes.add(2 * Process.pointerSize).readPointer(),
+            };
 
-            const Buffer = ObjectName.add(Process.pointerSize).readPointer();
-            this.FileName = Buffer.readUtf16String();
+            this.FileName = new UNICODE_STRING(OBJECT_ATTRIBUTES.ObjectName);
         }
     },
     onLeave: function (retval) {
@@ -59,15 +76,15 @@ Interceptor.attach(Module.getExportByName('ntdll.dll', 'ZwCreateFile'), {
         /* INVALID_HANDLE_VALUE */
         if (FileHandle.equals(-1) || FileHandle.equals(0)) return;
 
-        cfg.opened_files.set(Number(FileHandle), this.FileName);
+        cfg.opened_files.set(Number(FileHandle), this.FileName.toString());
 
-        console.log(`open(${has_name(FileHandle)}) -> ${FileHandle.toString(16)}`);
+        console.log(`open(${cfg.has_name(FileHandle)}) -> ${FileHandle.toString(16)}`);
     }
 });
 
 Interceptor.attach(Module.getExportByName('kernel32.dll', 'ReadFile'), {
     onEnter: function (args) {
-        this.currentPointer = current_pointer(args[0]);
+        this.currentPointer = utils.current_pointer(args[0]);
         this.hFile = args[0];
 
         this.bufferSize = Number(args[2]); this.pbufferSize = args[3];
@@ -82,7 +99,7 @@ Interceptor.attach(Module.getExportByName('kernel32.dll', 'ReadFile'), {
 
         console.log(
             `read<${this.currentPointer.toString(16)}>` + 
-            `(${has_name(this.hFile)}, ` + 
+            `(${cfg.has_name(this.hFile)}, ` + 
             `..., ${this.bufferSize}, ${realBufferSize})`);
 
         if (0 === realBufferSize || 0 === cfg.preview_length) return;
@@ -95,7 +112,7 @@ Interceptor.attach(Module.getExportByName('kernel32.dll', 'ReadFile'), {
 
 Interceptor.attach(Module.getExportByName('kernel32.dll', 'WriteFile'), {
     onEnter: function (args) {
-        this.currentPointer = current_pointer(args[0]);
+        this.currentPointer = utils.current_pointer(args[0]);
         this.hFile = args[0];
 
         this.bufferSize = Number(args[2]); this.pbufferSize = args[3];
@@ -110,7 +127,7 @@ Interceptor.attach(Module.getExportByName('kernel32.dll', 'WriteFile'), {
 
         console.log(
             `write<${this.currentPointer.toString(16)}>` + 
-            `(${has_name(this.hFile)}, ` + 
+            `(${cfg.has_name(this.hFile)}, ` + 
             `..., ${this.bufferSize}, ${realBufferSize})`);
 
         if (0 === realBufferSize || 0 === cfg.preview_length) return;
@@ -121,44 +138,43 @@ Interceptor.attach(Module.getExportByName('kernel32.dll', 'WriteFile'), {
     }
 });
 
+/*
+Interceptor.attach(Module.getExportByName('kernel32.dll', 'DeviceIoControl'), {
+    onEnter: function (args) {
+        this.hFile = args[0];
 
-// Interceptor.attach(Module.getExportByName('kernel32.dll', 'DeviceIoControl'), {
-//     onEnter: function (args) {
-//         this.hFile = args[0];
+        this.ioctl = Number(args[1]);
+        this.inBufferSize = Number(args[3]);
+        this.outBufferSize = Number(args[5]); this.pbufferSize = args[6];
 
-//         this.ioctl = Number(args[1]);
-//         this.inBufferSize = Number(args[3]);
-//         this.outBufferSize = Number(args[5]); this.pbufferSize = args[6];
+        this.inBuffer = args[2]; this.outBuffer = args[4];
+    },
+    onLeave: function (retval) {
+        if (retval.equals(0)) return;
 
-//         this.inBuffer = args[2]; this.outBuffer = args[4];
-//     },
-//     onLeave: function (retval) {
-//         if (retval.equals(0)) return;
+        const realBufferSize = this.pbufferSize.equals(0)
+            ? this.outBufferSize : this.pbufferSize.readU32();
 
-//         const realBufferSize = this.pbufferSize.equals(0)
-//             ? this.outBufferSize : this.pbufferSize.readU32();
+        console.log(
+            `ioctl(${cfg.has_name(this.hFile)}, ` +
+            `${this.ioctl.toString(16)}, ` + 
+            `..., ${this.inBufferSize}, ..., ${this.outBufferSize}, ${realBufferSize})`);
 
-//         console.log(
-//             `ioctl(${has_name(this.hFile)}, ` +
-//             `${this.ioctl.toString(16)}, ` + 
-//             `..., ${this.inBufferSize}, ..., ${this.outBufferSize}, ${realBufferSize})`);
+        if (0 === cfg.preview_length) return;
 
-//         if (0 === cfg.preview_length) return;
+        if (0 < this.inBufferSize) {
+            console.log('IN:  \n' +
+                hexdump(this.inBuffer.readByteArray(-1 === cfg.preview_length ? this.inBufferSize : cfg.preview_length),
+                { offset: 0, header: false, ansi: true }));
+        } else console.log('IN:  NaN')
 
-//         /* IN */
-//         if (0 < this.inBufferSize) {
-//             console.log(
-//                 hexdump(this.inBuffer.readByteArray(-1 === cfg.preview_length ? this.inBufferSize : cfg.preview_length),
-//                 { offset: 0, header: false, ansi: true }));
-//         } else console.log('IN:  NaN')
+        console.log('--------------------------------' + '--------------------------------');
 
-//         console.log('--------------------------------' + '--------------------------------');
-
-//         /* OUT */
-//         if (0 < realBufferSize) {
-//             console.log(
-//                 hexdump(this.outBuffer.readByteArray(-1 === cfg.preview_length ? realBufferSize : cfg.preview_length),
-//                 { offset: 0, header: false, ansi: true }));
-//         } else console.log('OUT:  NaN')
-//     }
-// });
+        if (0 < realBufferSize) {
+            console.log('OUT:  \n' +
+                hexdump(this.outBuffer.readByteArray(-1 === cfg.preview_length ? realBufferSize : cfg.preview_length),
+                { offset: 0, header: false, ansi: true }));
+        } else console.log('OUT:  NaN')
+    }
+});
+*/
